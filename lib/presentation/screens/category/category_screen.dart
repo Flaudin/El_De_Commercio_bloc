cef37a414f6540b597cc2f733c647c1dd548da62
Flaudin <94101364+Flaudin@users.noreply.github.com>
2025-03-31 16:11:08 +0800

    feat.Added CategoryPage

--- lib/presentation/screens/category/category_screen.dart
+++ lib/presentation/screens/category/category_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tracking_app/utils/constants.dart';
 
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -10,6 +12,40 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    TextEditingController searchText = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: TextFormField(
+          controller: searchText,
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search),
+            labelText: "Search",
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.r),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8.r),
+            ),
+          ),
+        ),
+        actionsPadding: EdgeInsets.symmetric(horizontal: 4.w),
+        actions: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.r),
+              border: Border.all(color: kGreyColor, width: 1.5.sp),
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.local_fire_department_outlined,
+                color: kPrimaryColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Container(padding: EdgeInsets.symmetric(horizontal: 12.w),child: SingleChildScrollView(child: Column(),),),
+    );
   }
 }
